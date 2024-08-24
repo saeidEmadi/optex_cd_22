327ba1102cca8aec73bf4ea210224a66399cbd4c
saeid <emadisaeidabad@gmail.com>
2024-08-24 11:17:07 +0330

    optex LaserMeasurement

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <QCoreApplication>
 
+#include "optex_cd_22.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QString error;
+
+    optex_cd_22 *optexObj = new optex_cd_22();
+
+    optexObj->initial("COM5", 9600, QSerialPort::DataBits::Data8, QSerialPort::StopBits::OneStop, QSerialPort::Parity::NoParity);
+
+    qInfo() << optexObj->value(&error);
+
+    if (error != "")
+    {
+        qInfo() << error;
+    }
 
     return a.exec();
 }
